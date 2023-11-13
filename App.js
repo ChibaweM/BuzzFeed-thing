e3de1f55cf510fac085793a0235fc016954fb279
ChibaweM <chibawemkwenya@gmail.com>
2023-11-13 22:20:11 +0200

    updates

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-const observer = new IntersectionObserver((entries)=>{
-    console.log(entries);
-    entries.forEach((entry)=>{
-        entry.target.classList.toggle('show', entry.isIntersecting)
-    })
-},
-{
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("show", entry.isIntersecting);
+    });
+  },
+  {
     threshold: 0.5,
-})
+  }
+);
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
